8a239625e6c376a7a8f84bc3b33fa0837aa0c363
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-09-28 20:30:36 +0200

    Update coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 NAME=search.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
+alr exec -- lcov --quiet --base-directory . --directory . \
+   --no-external --ignore-errors gcov,unused \
+   --exclude '*/<unknown>' \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*' -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
